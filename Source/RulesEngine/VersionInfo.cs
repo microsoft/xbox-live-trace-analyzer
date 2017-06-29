4d211652a026b09154425d565e67e9870c81bb44
James Lenell <jameslen@microsoft.com>
2017-06-29 10:15:35 -0700

    Update VersionInfo.cs

--- Source/RulesEngine/VersionInfo.cs
+++ Source/RulesEngine/VersionInfo.cs
@@ -5,6 +5,6 @@ namespace XboxLiveTrace
 {
     class VersionInfo
     {
-        public const System.String Version = "2016.04.310426.012";
+        public const System.String Version = "1.0.0.0";
     }
 }
