5ff73b430b64d4291d038a3bf0ac2f3be4e13310
Paul Liu <jicailiu@microsoft.com>
2017-09-26 12:48:53 -0700

    Bug fixes (#11)
    * Fix Bug: 13295083 [LTA] MPSD Threshold is causing unnecessary red flags
    
    * Remove XR053 as it is not required anymore.
    
    * Correct burst limit for presence service.
    
    * Address CR feedback.

--- Source/Reports/CertWarningReport.cs
+++ Source/Reports/CertWarningReport.cs
@@ -19,7 +19,6 @@ namespace XboxLiveTrace
         public string Requirement;
         public string Remark;
         public string Intent;
-        public List<Violation> Violations;
     }
 
     internal class CertWarningReport : IReport
@@ -58,32 +57,6 @@ namespace XboxLiveTrace
                     Remark = "The rich presence strings (including localized versions) must be configured in the Xbox service. Then titles must set rich presence strings by calling the SetPresenceAsync or set_presence API. <br/><br/> For more information about rich presence strings, see “Rich Presence Strings Overview” in the Xbox One Development Kit or Xbox Application Development Kit documentation.",
                     Intent = "Provide up-to-date information regarding what users are doing in a title. It's a form of promotion for the title and promotes social interaction by giving other users context into what their friends are doing."
                 });
-            }
-
-            // If we saw sessiondirectory traffic, then we assume you're  using multiplayer.
-            bool multiplayerUsed = result.Any(r => r.Endpoint == "sessiondirectory.xboxlive.com" && r.RuleName == "CallRecorder");
-
-            if (multiplayerUsed)
-            {
-                //"XRName":"XR-053: Instrumentation of Online Multiplayer Events"
-                var x53results = result.Where(r => r.RuleName == "XR053Rule");
-
-                if (x53results.Count() > 0)
-                {
-                    var x53result = x53results.First();
-
-                    if (x53result.ViolationCount > 0)
-                    {
-                        warningResults.Add(new CertWarning
-                        {
-                            XRName = "XR-053: Instrumentation of Online Multiplayer Events",
-                            Requirement = "All games that support online multiplayer gameplay must properly instrument the events of the class IGMRS and IGMRE, commonly known as MultiplayerRoundStart and MultiplayerRoundEnd common event schemas.",
-                            Remark = "Events that use the MultiplayerRoundStart and MultiplayerRoundEnd event schemas are used to compute user reputation and multiplayer usage reporting, including the number of multiplayer sessions played and the cumulative duration of sessions. <br/><br/> Titles must log the MultiplayerRoundStart event only when the user is engaged in an online multiplayer game session or round. Multiplayer Game Session is defined as an instance of synchronous gameplay in which two or more users participate across unique Xbox 360 or Xbox One consoles. Titles must not log multiplayer round events during asynchronous gameplay session. The MultiplayerRoundEnd event should be called when the user is no longer engaged. For example, MultiplayerRoundStart should be used when a gameplay round begins, and MultiplayerRoundEnd at the end of the gameplay round. Lobby wait time before or after rounds must not be included. \r\n Every pair of MultiplayerRoundStart and MultiplayerRoundEnd common events must include a valid XUID and MultiplayerCorrelationID. The MultiplayerCorrelationID field has to be retrieved from the user’s current session from the MPSD service. <br/><br/> For details about the proper instrumentation of these events, see “Xbox Live and Online Services (in “Xbox Data Platform” > “Events” > “Required Events and Stats”) in the Xbox One Development Kit or Xbox Application Development Kit documentation.Please notice that if you rename and/or create a custom event on the IGMRS or IGMRE class, those events are subject to the same requirements as stated above.",
-                            Intent = "Properly instrumenting these events will ensure that users’ online reputations and multiplayer usage KPIs are accurately calculated for your title.",
-                            Violations = x53result.Violations
-                        });
-                    }
-                }
             }
 
             using (var output = new StreamWriter(Path.Combine(outputDirectory, m_json ? "certWarning.json" : "certWarning.js")))
