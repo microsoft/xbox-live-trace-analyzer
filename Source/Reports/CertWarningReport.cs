7b5459ed96cb5ad6ff1d4e3efc98deb5151d8267
Nathan Iskandar <54045034+natiskan@users.noreply.github.com>
2021-07-26 13:20:03 -0700

    remove XR-049 (#46)

--- Source/Reports/CertWarningReport.cs
+++ Source/Reports/CertWarningReport.cs
@@ -33,32 +33,6 @@ namespace XboxLiveTrace
         {
             List<CertWarning> warningResults = new List<CertWarning>();
 
-            // XR-049: Rich Presence
-            var x49results = result.Where(r => r.RuleName == "XR049Rule");
-            bool x49violation = false;
-            // If no results, treat as violation.
-            if (x49results.Count() == 0)
-            {
-                x49violation = true;
-            }
-            else
-            {
-                var x49result = x49results.First();
-                x49violation = x49result.ViolationCount > 0;
-            }
-
-            // Add XR 049 warning
-            if (x49violation)
-            {
-                warningResults.Add(new CertWarning
-                {
-                    XRName = "XR-049: Rich Presence",
-                    Requirement = "Games must update a user’s presence information to accurately reflect his or her state.",
-                    Remark = "The rich presence strings (including localized versions) must be configured in the Xbox service. Then titles must set rich presence strings by calling the SetPresenceAsync or set_presence API. <br/><br/> For more information about rich presence strings, see “Rich Presence Strings Overview” in the Xbox One Development Kit or Xbox Application Development Kit documentation.",
-                    Intent = "Provide up-to-date information regarding what users are doing in a title. It's a form of promotion for the title and promotes social interaction by giving other users context into what their friends are doing."
-                });
-            }
-
             using (var output = new StreamWriter(Path.Combine(outputDirectory, m_json ? "certWarning.json" : "certWarning.js")))
             {
                 if (!m_json)
