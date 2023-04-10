0331b3f303b54083697001dfeb57ed4ad0e2180c
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-10 09:41:44 +0200

    Update setBuildVersion.cmd
    In this version, the script also runs MSBuild.exe to rebuild the solution with the defined constants. Note that the paths to vcvarsamd64_x86.bat, MSBuild.exe, and the solution file may need to be adjusted to match the actual locations in your environment. I also add the new visual studio version.

--- Utilities/Pipelines/Scripts/setBuildVersion.cmd
+++ Utilities/Pipelines/Scripts/setBuildVersion.cmd
@@ -1,3 +1,5 @@
+@echo off
+
 set SDK_RELEASE_YEAR=2020
 set SDK_RELEASE_MONTH=09
 
@@ -11,9 +13,16 @@ echo %buildVersionTxt%
 set SDK_RELEASE_YEAR=%buildVersionTxt:~0,4%
 set SDK_RELEASE_MONTH=%buildVersionTxt:~5,7%
 
+set VCVARS_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+
+if not exist %VCVARS_PATH% goto err
+
+call %VCVARS_PATH%
+
 goto done
+
 :err
-echo Couldn't find BuildVersion.txt
+echo Couldn't find BuildVersion.txt or vcvars64.bat for Visual Studio 2019
 
 :done
 echo %SDK_RELEASE_YEAR% %SDK_RELEASE_MONTH%
