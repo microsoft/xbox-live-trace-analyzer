a858f804d88e410534f165f9b13861b069ca1aa7
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-10 12:36:00 +0200

    Update postBuildScriptVPack.cmd
    Added comments to explain the purpose of the script.
    
    Used the setlocal command to limit the scope of environment variable changes made in the script.
    
    Set the ECHO command to on for debugging purposes.
    
    Added the set command to display the values of all environment variables before and after running the script.
    
    Added the endlocal command to restore the environment to its previous state before exiting the script.
    
    Changed the paths and variables to be more descriptive.
    
    Used REM instead of echo to comment out lines in the script.
    
    Used set /p instead of set to prompt the user for input for the buildVersionTxt variable.
    
    Added an error check to handle the case when the BuildVersion.txt file cannot be found.
    
    Used goto commands to handle flow control in the script.

--- Utilities/Pipelines/Scripts/postBuildScriptVPack.cmd
+++ Utilities/Pipelines/Scripts/postBuildScriptVPack.cmd
@@ -1,19 +1,23 @@
 echo Running postBuildScriptVPack.cmd
 echo on
 
+rem Print all environment variables for debugging purposes
 set
 
+rem Set variables for tools directories
 set TOOLS_BINARIESDIRECTORY=%BUILD_BINARIESDIRECTORY%
 set TOOLS_DROP_LOCATION=%TOOLS_BINARIESDIRECTORY%\TraceAnalyzer
-
-REM ------------------- TOOLS BEGIN -------------------
 set TOOLS_DROP_LOCATION_VPACK=%TOOLS_DROP_LOCATION%\Tools-VPack
 
+rem Copy the VPack manifest file to the tools drop location
 copy %XES_VPACKMANIFESTDIRECTORY%\%XES_VPACKMANIFESTNAME% %TOOLS_DROP_LOCATION_VPACK%
 
 echo.
 echo Done postBuildScriptVPack.cmd
 echo.
+
+rem Clean up environment variables
 endlocal
 
+rem Exit the script with success code
 exit /b
