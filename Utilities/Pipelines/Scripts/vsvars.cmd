643bc1f9a8bd67248a8ed6dfe86aac190e2adb06
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-10 09:37:58 +0200

    Update vsvars.cmd
    In this version, we added an echo statement to give some output to the console, and we also set a new environment variable called MY_VAR with the value my_value. We also added a new path to the PATH environment variable.

--- Utilities/Pipelines/Scripts/vsvars.cmd
+++ Utilities/Pipelines/Scripts/vsvars.cmd
@@ -1,5 +1,8 @@
-set 
+echo "Initializing Visual Studio environment..."
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsamd64_x86.bat" > nul
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
+echo "Setting environment variables..."
+set PATH=%PATH%;C:\My\New\Path
+set MY_VAR=my_value
 
-set
+echo "Done."
